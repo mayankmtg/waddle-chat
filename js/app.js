a4eb86c28a793d47031fc28ba66423dcd0e61487
mayankmtg <mayank15056@iiitd.ac.in>
2017-07-01 17:14:25 +0530

    profile changes and signup

--- js/app.js
+++ js/app.js
@@ -52,13 +52,12 @@ Waddle.config(['$routeProvider', function($routeProvider){
 		.when('/profile',{
 			templateUrl: 'views/profile.html',
 			controller: 'waddleProfile',
-			resolve: {
-				// controller will not be loaded until $requireSignIn resolves
+			 resolve: {
+				// controller will not be loaded until $waitForSignIn resolves
 				// Auth refers to our $firebaseAuth wrapper in the factory below
 				"currentAuth": ["Auth", function(Auth) {
-					// $requireSignIn returns a promise so the resolve waits for it to complete
-					// If the promise is rejected, it will throw a $routeChangeError (see above)
-					return Auth.$requireSignIn();
+					// $waitForSignIn returns a promise so the resolve waits for it to complete
+					return Auth.$waitForSignIn();
 				}]
 			}
 		}).otherwise({
@@ -86,20 +85,7 @@ Waddle.controller('waddleAuth', ['$scope','$location', "Auth", function($scope, 
 		$location.path('/chat');
 	}
 	$scope.waddleSignup = function() {
-		$scope.message = null;
-		$scope.error = null;
-		// Create a new user
-		Auth.$createUserWithEmailAndPassword($scope.waddleEmail, $scope.waddlePassword)
-			.then(function(firebaseUser) {
-				$scope.waddleEmail = "";
-				$scope.waddlePassword="";
-				$scope.message = "Sign Up Successful!! You can now Sign In Easily";
-				$location.path('/profile');
-
-			}).catch(function(error) {
-				console.log(error);
-				$scope.error = error;
-			});
+		$location.path('/profile');
 	};
 	$scope.waddleSignin=function(){
 		$scope.error=null;
@@ -124,31 +110,65 @@ Waddle.controller('waddleHome', ['$scope', '$location', function($scope, $locati
 	}
 }]);
 
-Waddle.controller('waddleProfile', ['$scope','currentAuth','$firebaseArray', '$location',function($scope, currentAuth,$firebaseArray, $location){
+Waddle.controller('waddleProfile', ['$scope','Auth','$firebaseArray','$location',function($scope, Auth,$firebaseArray,$location){
+	$scope.auth = Auth;
+
+	$scope.auth.$onAuthStateChanged(function(firebaseUser) {
+      		if (firebaseUser) {
+	      		$scope.firebaseUser = firebaseUser;
+	      		$scope.waddleDisp();
+	      		console.log($scope.firebaseUser);
+		}
+    	});
+
 	var ref=firebase.database().ref().child('Users');
-	var users= $firebaseArray(ref);	
+	var ref2=firebase.database().ref().child('usernames');
 	$scope.$watch("waddleUname", function(){
 		$scope.validUname=true;
-		for ( var x in users){
-			console.log(x);
-			if(x.uName === $scope.waddleUname || $scope.waddleUname === ""){
-				$scope.validUname=false;
-				break;
+		ref2.once('value', function(snapshot){
+			var stringUname=""+$scope.waddleUname;
+			if (snapshot.hasChild(stringUname) || $scope.waddleUname=="") {
+				$scope.$apply(function() {
+					$scope.validUname=false;
+				});
 			}
-		}
+		})
 		console.log("Valid: "+ $scope.validUname);
 	});
+	$scope.waddleDisp= function(){
+
+	}
 	$scope.waddleRegister = function(){
-		users.$add({
-			uName: $scope.waddleUname,
-			uId: currentAuth.uid
-		});
-		$location.path('/auth');
+		$scope.message = null;
+		$scope.error = null;
+		// Create a new user
+		if($scope.waddlePassword === $scope.waddleConfirm){
+			Auth.$createUserWithEmailAndPassword($scope.waddleEmail, $scope.waddlePassword)
+				.then(function(firebaseUser) {
+					$scope.message = "Sign Up Successful!! You can now Sign In Easily";
+					ref.child(Auth.$getAuth().uid).child("name").set($scope.waddleName);
+					ref.child(Auth.$getAuth().uid).child("uname").set($scope.waddleUname);
+					ref2.child($scope.waddleUname).set(Auth.$getAuth().uid);
+					$location.path('/auth');
+
+				}).catch(function(error) {
+					console.log(error);
+					$scope.error = error;
+				});	
+		}
+		else{
+			$scope.error.code="Password Mismatch";
+			$scope.error.message="Please re-enter Passwords in both fields";
+			$scope.waddlePassword="";
+			$scope.waddleConfirm="";
+
+		}
+			
 	}
 	
 
 }]);
 
-Waddle.controller('waddleChat', ['$scope', function($scope, $firebaseArray){
+Waddle.controller('waddleChat', ['$scope','$firebaseArray', function($scope, $firebaseArray){
 
 }]);
